e88d58cce9a8f14c6848795df0264b7758a6b077
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-07-30 16:49:11 +0200

    fix(data): correct arg name of save func

--- lyscripts/data/utils.py
+++ lyscripts/data/utils.py
@@ -9,6 +9,6 @@ from lyscripts.decorators import check_output_dir_exists, log_state
 
 @log_state()
 @check_output_dir_exists
-def save_table_to_csv(output_path: Path, table: pd.DataFrame):
+def save_table_to_csv(file_path: Path, table: pd.DataFrame):
     """Save a ``table`` to ``output_path``."""
-    table.to_csv(output_path, index=None)
+    table.to_csv(file_path, index=None)
