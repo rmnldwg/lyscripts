d5f99509e156a08ddf714bda784e3863b822fdf9
YoelPH <yoelhaas@gmail.com>
2024-11-07 09:28:04 +0100

    small update

--- lyscripts/plot/simplex_plot.py
+++ lyscripts/plot/simplex_plot.py
@@ -78,52 +78,52 @@ def create_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def plot_2d_simplex(mixture_df, data):
-    _, bottom_ax = plt.subplots()
-    subsites = list(mixture_df.columns)
-    cluster_x = mixture_df.loc[0]
-    cluster_y = [0. for _ in subsites]
-    annotations = [f"{label}\n({num})" for label, num in num_patients.items()]
-    bottom_ax.scatter(
-        cluster_x, cluster_y,
-        s=[num for num in num_patients.values()],
-        c=list(generate_location_colors(subsites)),
-        alpha=0.7,
-        linewidths=0.,
-        zorder=10,
-    )
+# def plot_2d_simplex(mixture_df, data):
+#     _, bottom_ax = plt.subplots()
+#     subsites = list(mixture_df.columns)
+#     cluster_x = mixture_df.loc[0]
+#     cluster_y = [0. for _ in subsites]
+#     annotations = [f"{label}\n({num})" for label, num in num_patients.items()]
+#     bottom_ax.scatter(
+#         cluster_x, cluster_y,
+#         s=[num for num in num_patients.values()],
+#         c=list(generate_location_colors(subsites)),
+#         alpha=0.7,
+#         linewidths=0.,
+#         zorder=10,
+#     )
 
-    sorted_idx = cluster_components.argsort()
-    sorted_x = cluster_components[sorted_idx]
-    sorted_annotations = [annotations[i] for i in sorted_idx]
-    sorted_num = [list(num_patients.values())[i] for i in sorted_idx]
-    for i, (x, num, annotation) in enumerate(zip(sorted_x, sorted_num, sorted_annotations)):
-        bottom_ax.annotate(
-            annotation,
-            # sqrt, because marker's area grows linearly with patient num, not radius
-            xy=(x, np.sqrt(0.0000003 * num) * (- 1)**i),
-            xytext=(x, 0.025 * (- 1)**i),
-            ha="center",
-            va="bottom" if i % 2 == 0 else "top",
-            fontsize="small",
-            arrowprops={
-                "arrowstyle": "-",
-                "color": USZ["gray"],
-                "linewidth": 1.,
-            }
-        )
+#     sorted_idx = cluster_components.argsort()
+#     sorted_x = cluster_components[sorted_idx]
+#     sorted_annotations = [annotations[i] for i in sorted_idx]
+#     sorted_num = [list(num_patients.values())[i] for i in sorted_idx]
+#     for i, (x, num, annotation) in enumerate(zip(sorted_x, sorted_num, sorted_annotations)):
+#         bottom_ax.annotate(
+#             annotation,
+#             # sqrt, because marker's area grows linearly with patient num, not radius
+#             xy=(x, np.sqrt(0.0000003 * num) * (- 1)**i),
+#             xytext=(x, 0.025 * (- 1)**i),
+#             ha="center",
+#             va="bottom" if i % 2 == 0 else "top",
+#             fontsize="small",
+#             arrowprops={
+#                 "arrowstyle": "-",
+#                 "color": USZ["gray"],
+#                 "linewidth": 1.,
+#             }
+#         )
 
-    bottom_ax.set_xlabel("assignment to component A")
-    bottom_ax.xaxis.set_major_formatter(StrMethodFormatter("{x:.0%}"))
-    top_ax = bottom_ax.secondary_xaxis(
-        location="top",
-        functions=(lambda x: 1. - x, lambda x: 1. - x),
-    )
-    top_ax.set_xlabel("assignment to component B")
-    top_ax.xaxis.set_major_formatter(StrMethodFormatter("{x:.0%}"))
-    bottom_ax.set_yticks([])
-    bottom_ax.grid(axis="x", alpha=0.5, color=USZ["gray"], linestyle=":")
-    plt.savefig(args.output, bbox_inches="tight", dpi=300)
+#     bottom_ax.set_xlabel("assignment to component A")
+#     bottom_ax.xaxis.set_major_formatter(StrMethodFormatter("{x:.0%}"))
+#     top_ax = bottom_ax.secondary_xaxis(
+#         location="top",
+#         functions=(lambda x: 1. - x, lambda x: 1. - x),
+#     )
+#     top_ax.set_xlabel("assignment to component B")
+#     top_ax.xaxis.set_major_formatter(StrMethodFormatter("{x:.0%}"))
+#     bottom_ax.set_yticks([])
+#     bottom_ax.grid(axis="x", alpha=0.5, color=USZ["gray"], linestyle=":")
+#     plt.savefig(args.output, bbox_inches="tight", dpi=300)
 
 # Function to add perpendicular ticks as short lines
 def add_perpendicular_ticks(x1, y1, x2, y2, tick_length=0.01):
