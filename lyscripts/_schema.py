16e9b1ada8e9f3e7a3b8154953164542a1855da9
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-01-21 14:33:24 +0100

    feat: update YAML schema for CLIs

--- lyscripts/_schema.py
+++ lyscripts/_schema.py
@@ -2,21 +2,39 @@
 
 import json
 
-from lyscripts import sample
-from lyscripts.compute import priors
+from lydata.utils import ModalityConfig
+from pydantic import BaseModel, Field
+
+from lyscripts import configs
 
 
-class SchemaSettings(sample.SampleCLI, priors.PriorsCLI):
+class SchemaSettings(BaseModel):
     """Settings for generating a JSON schema for lyscripts configuration files."""
 
-    ...
+    version: int = Field(
+        description=(
+            "Version of the configuration. Must conform to the major version of the "
+            "lyscripts package (can only be 1 at the moment). This is used to avoid "
+            "compatibility issues when the configuration format changes."
+        ),
+        ge=1,
+        le=1,
+    )
+    cross_validation: configs.CrossValidationConfig = None
+    data: configs.DataConfig = None
+    diagnosis: configs.DiagnosisConfig = None
+    distributions: dict[str, configs.DistributionConfig] = {}
+    graph: configs.GraphConfig = None
+    involvement: configs.InvolvementConfig = None
+    modalities: dict[str, ModalityConfig] = {}
+    model: configs.ModelConfig = None
+    sampling: configs.SamplingConfig = None
+    scenarios: list[configs.ScenarioConfig] = []
 
 
 def main() -> None:
     """Generate a JSON schema for lyscripts configuration files."""
     schema = SchemaSettings.model_json_schema()
-    # Different cmds may require different settings, so we remove the required field.
-    schema.pop("required")
     print(json.dumps(schema, indent=2))  # noqa: T201
 
 
